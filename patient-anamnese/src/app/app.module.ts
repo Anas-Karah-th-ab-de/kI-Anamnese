6d6cda76facdb3c09af1c919923b59ca886d6335
Anas-Karah-th-ab-de <s210835@th-ab.de>
2023-04-11 22:56:26 +0200

    erstellung

--- patient-anamnese/src/app/app.module.ts
+++ patient-anamnese/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PatientFormComponent } from './patient-form/patient-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientFormComponent
   ],
   imports: [
     BrowserModule
